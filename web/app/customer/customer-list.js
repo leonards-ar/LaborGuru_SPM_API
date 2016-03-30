3d961fbd619ed687710f8ef256afe712e8e3aeda
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2016-03-30 01:08:20 +0000

    Adding showInfo window

--- web/app/customer/customer-list.js
+++ web/app/customer/customer-list.js
@@ -1,14 +1,12 @@
-angular.module('web').controller('CustomerListCtrl',function($scope,$http,customerService, $location, $window,$log){
+angular.module('web').controller('CustomerListCtrl',function($scope,$modal,customerService, $location, $window,$log){
 
 var vm = $scope;
 vm.dataLoading = false;
 vm.customerList = {};
 
-$log.log("!!!!!!Estoy aca !!!!!!!!!!!");
 customerService.getCustomers()
 				       .then(function(res){
-                 $log.log("Successful!!! " + res.length);
-				    	   vm.customerList = res;
+                 vm.customerList = res;
                },function(res){
                  $log.log(res.data || "Request Failed");
                }
@@ -18,6 +16,36 @@ customerService.getCustomers()
 //   vm.customerList = response.data;
 // });
 
+vm.showCustomer = function(customerId){
+  var promise = customerService.loadCustomer(customerId);
+  promise.then(function(customer){
 
+    var modalCustomerInfo = $modal.open({
+          animation: true,
+          templateUrl: 'app/customer/customer-info.html',
+          controller: 'CustomerInfoCtrl',
+          size: "lg",
+          resolve: {
+            showInfo: function() {
+              return {
+                "name": customer.name,
+                "code": customer.code,
+                "regions": customer.regionDto
+              };
+            }
+          }
+      });
+    },function(res){
+      $log.log(res.data || "Request Failed");
+    }
+    );
+  };
 
+  vm.remove  = function(customerId){
+    var promise = customerService.removeCustomer(customerId);
+    promise.then(function(){
+      vm.message = "Customer deleted";
+      rootScope.showSuccessAlert = true;
+    });
+  }
 });
