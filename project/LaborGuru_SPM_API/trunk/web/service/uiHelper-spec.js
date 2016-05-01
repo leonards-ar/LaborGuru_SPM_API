6598b38223b791241d310273905e9972bf651a67
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2016-05-01 03:16:10 +0000

    Adding helper

---
+++ project/LaborGuru_SPM_API/trunk/web/service/uiHelper-spec.js
@@ -0,0 +1,11 @@
+describe('uiHelper', function() {
+
+  beforeEach(module('web'));
+
+  it('should ...', inject(function(uiHelper) {
+
+	//expect(uiHelper.doSomething()).toEqual('something');
+
+  }));
+
+});
